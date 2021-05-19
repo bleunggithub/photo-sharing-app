cb79db598301254eb9ffe39bb2488a2dfb702d72
B Leung <leungcheukkei@gmail.com>
2021-05-19 16:24:52 +0800

    fix cross site clear cookie issue

--- instapic-server/services/logout.js
+++ instapic-server/services/logout.js
@@ -1,5 +1,5 @@
 
 module.exports.logout = async (req, res) => {
-        res.clearCookie('zed')
+        res.clearCookie('zed', {sameSite: 'none', secure: true} )
         res.status(200).json({message: 'logged out successfully.'})
 }
